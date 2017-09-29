fc6a27b61e35f127681e3b7fd698ba393003fec6
DEVIN TASSIE <devintassie@DEVINs-MacBook-Pro.local>
2017-09-29 18:53:55 -0500

    checkpoint2

--- app.js
+++ app.js
@@ -1,8 +1,53 @@
-window.onload = function() {
-   alert("The window has loaded!");
-  onReady();
- };
- function onReady() {
+function onReady() {
+   var toDos = [];
+    var addToDoForm = document.getElementById('addToDoForm');
+
+
+   function createNewToDo() {
+var newToDoText = document.getElementById('newToDoText');
+
+toDos.push({
+      title: newToDoText.value,
+      complete: false
+    });
+
+    newToDoText.value = '';
+ }
+
+ renderTheUI(toDos);
+}
+
+
+ addToDoForm.addEventListener('submit', function(event) {
+    event.preventDefault();
+    createNewToDo();
+  });
+
+  function renderTheUI(toDos) {
+      var toDoList = document.getElementById('toDoList');
+
+      toDoList.innerHTML = '';
+
+      toDos.forEach(function(toDo) {
+        var newLi = document.createElement('li');
+        var checkbox = document.createElement('input');
+        checkbox.type = "checkbox";
+
+        newLi.innerHTML = toDo.title;
+
+        toDoList.appendChild(newLi);
+        newLi.appendChild(checkbox);
+      });
+
+    }
+  }
+
+  window.onload = function() {
+     alert("The window has loaded!");
+    onReady();
+
+   };
+
    const addToDoForm = document.getElementById('addToDoForm');
    const newToDoText = document.getElementById('newToDoText');
    const toDoList = document.getElementById('toDoList');
