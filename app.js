84995cc2fd50caa61f683d687fb29e7136a97404
DEVIN TASSIE <devintassie@DEVINs-MacBook-Pro.local>
2017-10-10 15:15:03 -0500

    assignment attmept

--- app.js
+++ app.js
@@ -1,80 +1,43 @@
 function onReady() {
-   var toDos = [];
-    var addToDoForm = document.getElementById('addToDoForm');
+    const toDos = [];
+    const addToDoForm = document.getElementById('addToDoForm');
+       addToDoForm.addEventListener('submit',(event) => {
+       event.preventDefault();
+       createNewToDo();
+     });
+}
 
-
-   function createNewToDo() {
-var newToDoText = document.getElementById('newToDoText');
+function createNewToDo() {
+   var newToDoText = document.getElementById('newToDoText');
+   if (!newToDoText) { return }
 
 toDos.push({
       title: newToDoText.value,
       complete: false
     });
 
-    newToDoText.value = '';
- }
-
- renderTheUI(toDos);
+  newToDoText.value = '';
 }
 
+function renderTheUI(toDos) {
 
- addToDoForm.addEventListener('submit', function(event) {
-    event.preventDefault();
-    createNewToDo();
-  });
+      const toDoList = document.getElementById('toDoList');
 
-  function renderTheUI(toDos) {
-      var toDoList = document.getElementById('toDoList');
-
-      toDoList.innerHTML = '';
+      toDoList.textContent = '';
 
       toDos.forEach(function(toDo) {
-        var newLi = document.createElement('li');
-        var checkbox = document.createElement('input');
+        const newLi = document.createElement('li');
+        const checkbox = document.createElement('input');
         checkbox.type = "checkbox";
 
-        newLi.innerHTML = toDo.title;
+
+        newLi.textContent = toDo.title;
 
         toDoList.appendChild(newLi);
         newLi.appendChild(checkbox);
       });
+   }
 
-    }
-  }
-
-  window.onload = function() {
-     alert("The window has loaded!");
-    onReady();
-
-   };
-
-   const addToDoForm = document.getElementById('addToDoForm');
-   const newToDoText = document.getElementById('newToDoText');
-   const toDoList = document.getElementById('toDoList');
-
-   addToDoForm.addEventListener('submit', () => {
-event.preventDefault();
-
-    // get the text
-    let title = newToDoText.value;
-
-    // create a new li
-    let newLi = document.createElement('li');
-
-    // create a new input for the checkbox
-    let checkbox = document.createElement('input');
-
-    // set the input's type to checkbox
-    checkbox.type = "checkbox";
-
-    // set the title
-    newLi.textContent = title;
-
-    // attach the checkbox to the li
-    newLi.appendChild(checkbox);
-
-    //empty the input
-    newToDoText.value = '';
-
-  });
- }
+ window.onload = function() {
+        onReady();
+    };
